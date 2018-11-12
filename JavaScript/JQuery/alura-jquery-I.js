14dd905e92e7d5ba7da012b24e47c0c4532c5f26
juanoude <juanoude@gmail.com>
2018-11-12 21:55:07 -0200

    Um pouquinho mais de JQuery

--- JavaScript/JQuery/alura-jquery-I.js
+++ JavaScript/JQuery/alura-jquery-I.js
@@ -1,3 +1,5 @@
+//Aula 01
+
 var frase = jQuery(".frase").text(); //jQuery == $
 var numPalavras = frase.split(" ").length(); // quebra em um array pelos espaços
 console.log(numPalavras); //escreve no console
@@ -7,3 +9,20 @@ var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
 $("li"); //seleciona todos os li's da página
+
+//Aula 02
+
+<textarea class="campo-digitacao" rows="8" cols ="40"></textarea>
+ <li><span id="contador-caracteres">0</span> caracteres</li>
+ <li><span id="contador-palavras">0</span> palavras</li>
+
+var campo = $(".campo-digitacao");
+campo.on('input', function(){
+  var conteudo = campo.val();//val pega o conteudo inserido em inputs
+  var conteudoSemEspaco = conteudo.replace("/\s+/g",'');//Desconsiderando espaços como caracteres;
+  var qtdCaracteres = conteudoSemEspaco.length;
+  var qtdPalavras = conteudo.split(/\S+/).length - 1; //A expressão regular desconsidera varios espaços como uma série de palavras;
+  conteudo.replace(/\s+/g,'').length;
+  $("contador-caracteres").text(qtdCaracteres);
+  $("contador-palavras").text(qtdPalavras);
+});
