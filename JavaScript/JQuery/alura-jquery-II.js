5a35bbc55e28436aede75c0b5a647c3677cfe768
juanoude <juanoude@gmail.com>
2018-11-27 22:01:50 -0200

    JQuery2 - AJAX

--- JavaScript/JQuery/alura-jquery-II.js
+++ JavaScript/JQuery/alura-jquery-II.js
@@ -1,4 +1,4 @@
-//Aula 01
+//Aula 01 - Animações
 
 //dado o novo botão escondido:
 <a id="botao-placar" class="btn-floating btn-large waves-effect waves-light green">
@@ -70,3 +70,80 @@ $('li').dblclick(function() { //shorthand do doubleclick
     this.remove();
   })
 });
+
+
+
+//Aula 02 - AJAX
+
+//dado o botão:
+<a id="botao-frase" class="btn-floating btn-large waves-effect waves-light blue">
+  <i class="material-icons">shuffle</i>
+</a>
+
+$("#botao-frase").click(fraseAleatoria);
+
+//subindo um servidor que cospe um json numa determinada rota
+/*no caso: http://localhost:3000/frases - que possui 10 frases (id, texto, tempo)*/
+
+function fraseAleatoria(){
+  $.get("http://localhost:3000/frases", trocarFraseAleatoria);
+}
+
+function trocarFraseAleatoria(data){
+  var frase = $(".frase");
+  var numeroAleatorio = Math.floor(Math.random() * data.length);
+  frase.text(data[numeroAleatorio].texto);
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(data[numeroAleatorio].tempo)
+}
+
+function atualizaTempoInicial(tempo) {
+    $("#tempo-digitacao").text(tempo);
+}
+
+//consertar o bug (carregando o tempo e frase de comparação no evento e não ao carregar a página)
+function inicializaMarcadores() {
+  campo.on("input", function() {
+    var frase = $(".frase").text();
+    var tempoRestante = $("#tempo-digitacao").text();
+    // ...
+  });
+}
+
+
+
+//Aula 03
+
+//dado o erro e spinner:
+<div class="center">
+  <p id="erro">Ocorreu um erro, por favor tente novamente!</p>
+</div>
+<div class="center">
+  <img src="img/spinner.gif" alt="Spinner" id="spinner">
+  <p id="erro">Ocorreu um erro, por favor tente novamente</p>
+</div>
+
+#erro{
+    color: red;
+    display: none;
+}
+
+#spinner{
+    display: none;
+}
+
+//exibindo msg de erro e spinner de carregamento:
+function fraseAleatoria() {
+  $("#spinner").show();
+
+  $.get("http://localhost:3000/frases2222", trocaFraseAleatoria) //URL errada para simular um problema
+  .fail(function(){
+    $("#erro").show(); //ao falhar mostra a mensagem de erro
+    setTimeOut(function(){
+      $("#erro").hide();
+    },1500);
+  })
+  .always(function(){
+    $("#spinner").hide();
+  });
+}
