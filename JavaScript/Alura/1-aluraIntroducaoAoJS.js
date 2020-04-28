a155a56cf6aa32b3d43260c393e4ead7e5c8ce89
juanoude <juanoude@hotmail.com>
2020-04-28 17:27:31 -0300

    primeiros passos com JS

--- JavaScript/Alura/1-aluraIntroducaoAoJS.js
+++ JavaScript/Alura/1-aluraIntroducaoAoJS.js
@@ -13,10 +13,78 @@ console.log("Ricardo" / 2); // NaN (Not a Number)
 console.log(6.5); //6.5
 console.log(6,5); //6 5
 
+//Declaração de Variáveis.
 //Manter o máximo de constantes é uma BOA PRÁTICA!
 const constante = "Não é possível reatribuir valor, em objetos, não se muda a estrutura";
 let naoConstante = "Já com o let, ";
 naoConstante = naoConstante + "é possível reatribuir o valor da variável";
 
-//Os Nomes de Variáveis devem ser o mais explicito/explicativo Possível mesmo que
-//fique extenso - BOA PRÁTICA
+//o let pode ser declarado e atribuído em linhas diferentes, o const deve ser na mesma linha:
+let vouDeclarar;
+vouDeclarar = "declarei";
+
+const jaDeclarei = 'Declarado';
+
+/* Os Nomes de Variáveis devem ser o mais explicito/explicativo Possível mesmo que
+fique extenso - BOA PRÁTICA */
+
+
+//Arrays
+const cidades = new Array(
+  'São Paulo',
+  'Curitiba',
+  'Brasília'
+);
+
+cidades.push('Florianópolis'); //deixa a estrutura constante, mas pode-se adicionar...
+cidades.splice(1, 1); //..., deletar e diversos outros métodos para arrays
+
+
+//Condicionais:
+if (condicao > 18) {
+  console.log('true');
+} else {
+  console.log('false');
+}
+
+if (condicao > 18) {
+  console.log('true');
+} else if (outraCondicao) {
+  console.log('false - true');
+} else {
+  console.log('false - false');
+}
+
+if(salario < 2600.0)
+  console.log("A sua aliquota é de 15%"); //Está dentro
+  console.log("Você pode deduzir até R$ 350"); //Está fora
+
+// || == ou
+// && == e
+
+//Iteradores
+const destino = 'Brasília';
+let existsDest = false;
+let contador = 0;
+while (contador < 3) {
+  if (cidades[contador] == destino) {
+    console.log('existe no array');
+    existsDest = true;
+    break;
+  }
+  contador += 1;
+}
+console.log('destino existe:', existsDest);
+
+//no for:
+for (i = 0; i < 3; i++) {
+  if (cidades[i] === destino) {
+    console.log('existe no array');
+    existsDest = true;
+    break;
+  }
+}
+console.log('destino existe:', existsDest);
+
+//Debugger - Continue vai para no próximo breakpoint e step over executa a próxima linha.
+//É possível criar várias configurações de debug com o launch.json
