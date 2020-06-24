d74b714c54d07a271da5bca08f7494d73d55cc72
juanoude <juanoude@hotmail.com>
2020-06-24 20:15:46 -0300

    RegEx

--- JavaScript/Alura/5-RegEx.js
+++ JavaScript/Alura/5-RegEx.js
@@ -92,3 +92,68 @@ Ex: ^file.+\.html$ // A html file url. Begins with file and ends with html.
 //Exercises:
 ^Caused by:.+ //Getting errors that always start with 'Caused by:'
 ^Data:[\s]?\d{2}/\d{2}/\d{4}$ //Getting dates with the structure 'Data: dd/mm/yyyy' or 'Data:dd/mm/yyyy'
+
+
+//------------------------------------------------------------------------------
+//Lesson 4
+
+//Imagine that you want to search a pattern, but wants only a piece of that pattern
+//as the result, this is possible with the grouping tool:
+() //Groups a piece of the Regex
+Ex: (a)a{3} // Pick the first 'a' of 'aaaa'
+
+//You can define the optional quantifier to entire groups
+Ex: a(a{2}\s)?a{2} // Pick 'aaa' or 'aaa aa'
+
+//You can define groups that you don't want to return as part of the result:
+(?:denied) //Means that you don't want that group as a result
+Ex: a(?:a{2})?a{2} // The return will match the pattern in this case.
+
+//Exercises:
+([0123]?\d)\s+(?:de\s+)?([A-Z][a-zç]{1,8})\s+(?:de\s+)?([12]\d{3}) // Get dd Month yyyy from a date
+\d{3}[-.]?\d{3}[.-]?\d{3}[.-]?(\d{2}) //Get the last 2 numbers of CPF
+[Z]\d+(\w) // Get a word from the cipher
+
+//Given the error Caused by: com.mysql.jdbc.exceptions.jdbc4.CommunicationsException: Communications link failure
+(Caused[\s\w:.]+):([\w\s]+) //Gives the path and the message in two groups
+
+//Given the emails 'super.mario@caelum.com.br' 'donkey.kong@alura.com.br' 'bowser1@alura.com.br'
+([a-z.]{4,14}[a-z\d]?)@(?:alura.com.br|caelum.com.br)// Get the emails names.
+
+//Given the should match emails 'donkey.kong@kart.com.br' 'bowser1@games.info' 'super-mario@nintendo.JP' 'TEAM.donkey-kong@MARIO.kart1.nintendo.com'
+//and the should not match emails 'wario@kart@nintendo.com' 'yoshi@nintendo' 'daisy@nintendo.b' '..@email.com'
+^([-\w])+(\.[-\w]+)?@(\w+\.)+([a-zA-Z]{2,}) //gives the expected result.
+// In the course answer was: ^([\w-]\.?)+@([\w-]+\.)+([A-Za-z]{2,4})+$
+
+//Given the adresses:
+// Nico Steppat|14/05/1977|Rua Buarque de Macedo|50|22222-222|Rio de Janeiro
+// Romulo Henrique|14/06/1993|Rua do Lins|120|12345-322|Rio de Janeiro Leonardo
+// Cordeiro|01/01/1995|Rua de Campo Grande|01|00001-234|Rio de Janeiro
+([\w\s]+)\|(?:\d+/){2}\d+\|([\w\s]+)\|(\d+)\|(\d{5}-\d{3})\|(?:\w+\s?)+
+//Gets the name, street, number and CEP
+
+
+//------------------------------------------------------------------------------
+//Lesson 5
+
+//The . metachar works in a way that it covers the max length he can
+//to revert this behavior and make it stop in the first ocurrence, use '?'
+//For example, given the string '<h1 class="text-left">Expressões regulares</h1>'
+<h1.+> //Will select '<h1 class="text-left">Expressões regulares</h1>'
+<h1.+?> //Will select '<h1 class="text-left">'
+
+//Given the world 'alura'
+[a-z]+ //Select one match as 'alura'
+[a-z]+? //Select five matches as 'a' 'l' 'u' 'r' 'a'
+
+| //Make a or operator
+(string1 | string2) //in group packs the whole word as a option
+
+//You can reference a object in the regex
+//For instance in the pattern '<(h1|h2).+?>([\w\sõãí.]+)</(h1|h2)>'
+//for the string '<h1 id="regex" class="form">Expressões </h2>'
+//the match for h1 in the first object and h2 in the end is valid
+<(h1|h2).+?>([\w\sõãí.]+)</\1> // in this one references the first object value
+
+//Exercises:
+<(p[1-9])> .*? </\1> //Check the tags and its respective closage <p1> to <p9>
