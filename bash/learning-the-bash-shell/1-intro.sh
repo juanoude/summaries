fc0f33dc41d5c81815eff635105715a05ab35125
Juan Ananda <juanoude@gmail.com>
2024-11-04 09:53:29 -0300

    changes: - some more bash practices

--- bash/learning-the-bash-shell/1-intro.sh
+++ bash/learning-the-bash-shell/1-intro.sh
@@ -30,4 +30,24 @@ ls *.{py,sh}
 echo {1..9}
 echo {a..z}
 
+## Standard I/O
+# Popular UNIX Data Filtering utilities
+cat # Copy input to output
+grep # Search for string in the input
+sort # Sort line in the input
+cut # Extract columns from the input
+sed # Perform editing operations on the input
+tr # Translate characters in the input to other characters
 
+# Examples:
+cat < file.txt # prints file contents on screen
+sort < file.txt # will sort the lines on the file
+date > now # saves the current date to a file called now
+cat < file1 > file2 # copies the contents of file1 to file2
+
+## Pipelines - command chaining
+ls -l | more # a screen at a time
+sort < file.txt | more
+# users formatting cam:LM1c7GhNesD4GhF3iEHrH4FeCKB/:501:100:Cameron Newham:/home/cam:/bin/bash
+# so you can list the systems users with:
+cut -d: -f1 < /etc/passwd | sort
