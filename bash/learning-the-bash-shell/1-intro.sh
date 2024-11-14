f25160dccd1bedd0f1fa62b4403417724c99dc07
Juan Ananda <juanoude@gmail.com>
2024-11-14 10:02:24 -0300

    changes: - adding multiline and control commands

--- bash/learning-the-bash-shell/1-intro.sh
+++ bash/learning-the-bash-shell/1-intro.sh
@@ -51,3 +51,63 @@ sort < file.txt | more
 # users formatting cam:LM1c7GhNesD4GhF3iEHrH4FeCKB/:501:100:Cameron Newham:/home/cam:/bin/bash
 # so you can list the systems users with:
 cut -d: -f1 < /etc/passwd | sort
+
+
+## Background Jobs
+uncompress gcc.tar.gz & # you will see a line like [1] 175
+# These numbers gives you ways to refer to the background job
+
+diff file1.txt file2.txt # diffs file
+diff file1.txt file2.txt & # diffs on background
+# if the files are big, it will pollute your terminal and be dificult to stop
+diff file1.txt file2.txt > txtdiff & # diffs on background and saves on a log
+# every job has a priority, to lower the priority of a job you can use
+nice command
+
+## Special Characters
+# we already saw < > & | * ? [] {}
+# ~ -> home directory
+# ` -> command substitution
+# # -> comment
+# $ -> variable
+# () -> subshell
+# \ -> escape
+# ; -> command separator
+# <"> -> weak quoting
+# / -> path separator
+# ! -> logical not
+
+## Quoting
+echo 2 * 3 > 5 is a valid inequality # creates a file named 5
+echo '2 * 3 > 5 is a valid inequality' # creates a string
+# double quotes still treats some special characters as special (weak quoting)
+## Escaping
+echo 2 \* 3 \> 5 is a valid inequality 
+echo \"2 \* 3 \> 5\" is a valid inequality # printing quotes
+echo 'Hatter\'s tea Party' ' # this escape doesn`t work
+echo 'Hatter'\''s tea Party' # this escape works
+# To break line you just need to quote the return key
+echo This is a phrase that contains several lines \
+The return key is just another character you should just escape it \
+and you should be good to go my fellow students
+# Or you can just use a quotation
+echo 'Some multiline
+string, this works just fine'
+
+## Control Keys
+erase   kill    werase   rprnt  flush   lnext   susp    intr   quit   stop    eof
+^?      ^U      ^W       ^R     ^O      ^V      ^Z/^Y   ^C     ^\     ^S/^Q   ^D
+
+CTRL-C - intr - Stop current command
+CTRL-D - eof  - End of input
+CTRL-\ - quit - Stop current command if CTRL-C doesn't work
+CTRL-S - stop - Halt output to screen
+CTRL-Q - start - Restart output to screen
+DEL or CTRL-? - erase - Erase last character
+CTRL-U - kill - Erase entire command line
+CTRL-Z - susp - Suspend current command
+
+stty -a # shows terminal control keys
+help cd # shows info on bash commands
+
+
